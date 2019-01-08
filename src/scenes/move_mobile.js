22d886e6ab24b125b70e48efe939c2a29d5c2a1e
Gunner <voragun2543@gmail.com>
2019-01-08 12:39:52 +0700

    [ADD]touch_complete
    ทำได้ละครับ น้ำตาจิไหล

--- src/scenes/move_mobile.js
+++ src/scenes/move_mobile.js
@@ -2,6 +2,7 @@ import 'phaser';
 
 let phasers;
 let player;
+let upButton = false;
 let up;
 let down;
 let right;
@@ -22,29 +23,59 @@ class move_mobile extends Phaser.Scene {
         this.load.image('up', './../../images/button_up.png');
         this.load.image('down', '../../images/button_down.png');
         this.load.image('left', '../../images/button_left.png');
-        this.load.image('right','../../images/button_right.png');
-        
-        this.load.image('player','../../images/player.png');
+        this.load.image('right', '../../images/button_right.png');
+
+        this.load.image('player', '../../images/player.png');
     }
-    create() {      
+    create() {
         //กำหนดตัวละครเป็น physics
-        player = phasers.physics.add.sprite(73, 72, 'player').setScale(0.5);      
+        player = phasers.physics.add.sprite(73, 72, 'player').setScale(0.5);
 
         //ไม่ให้ player ออกนอกโลก
         player.setCollideWorldBounds(true);
 
-        const upButton = phasers.add.image(100, 500, 'up');
-        upButton.setInteractive();
-    
-        upButton.on('pointerover', () => { console.log('up'); });
+        //ใส่ปุ่มในมือถือ
+        phasers.rightButton = phasers.add.image(150, 500, 'right').setInteractive();
+        phasers.rightButton.on('pointerdown', control_right);
+        phasers.rightButton.on('pointerup', control_stopX);
 
+        phasers.lefttButton = phasers.add.image(50, 500, 'left').setInteractive();
+        phasers.lefttButton.on('pointerdown', control_left);
+        phasers.lefttButton.on('pointerup', control_stopX);        
+
+        phasers.upButton = phasers.add.image(100, 450, 'up').setInteractive();
+        phasers.upButton.on('pointerdown', control_up);
+        phasers.upButton.on('pointerup', control_stopY);   
+        
+        phasers.downButton = phasers.add.image(100, 550, 'up').setInteractive();
+        phasers.downButton.on('pointerdown', control_down);
+        phasers.downButton.on('pointerup', control_stopY);   
+        
+        function control_right(){
+            player.setVelocityX(150);
+        }
+        function control_left(){
+            player.setVelocityX(-150);
+        }
+        function control_up(){
+            player.setVelocityY(-150);
+        }
+        function control_down(){
+            player.setVelocityY(150);
+        }
+
+        function control_stopX(){
+            player.setVelocityX(0);
+        }
+        function control_stopY(){
+            player.setVelocityY(0);
+        }
     }
 
     update() {
-        
+
+
     }
-
-
 
 
 }
